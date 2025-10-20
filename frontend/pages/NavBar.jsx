41cf6e0cbed97f9166b906f10cffdaf9bcc0d7a4
Richa Gupta <richamgupta2005@gmail.com>
2025-10-20 14:27:14 +0530

    Navbar and Routes Linked

--- frontend/pages/NavBar.jsx
+++ frontend/pages/NavBar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import '../styling/Navbar.css';
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <>
+      <div className='navbar'>
+        <div >
+          <Link to='/'><img src="/assets/logo.png" className='logoPic'></img></Link>
+        </div>
+      <div>
+        <Link to="/" className = "pagesLink">Home</Link>
+        <Link to="/medical" className = "pagesLink" >Medical</Link>
+        <Link to="/life" className = "pagesLink">Life</Link>
+        <Link to="/car" className = "pagesLink">Car</Link>
+      </div>
+      <Link to="/profile">
+        <img src="/assets/profile.png" className="profilePic"/>
+      </Link>
+      </div>
+    </>
   )
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import {  Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage.jsx';
 import NavBar from '../pages/NavBar.jsx';
+import MedicalPage from '../pages/MedicalPage.jsx';
+import CarPage from '../pages/CarPage.jsx';
+import LifePage from '../pages/lifePage.jsx';
+import ProfilePage from '../pages/ProfilePage.jsx';
 export default function App() {
   return (
     <>
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/" element={<MoviePage />} />
-          <Route path="/suggest" element={<SuggestionPage />} /> */}
+          <Route path="/medical" element={<MedicalPage />} />
+          <Route path="/car" element={<CarPage />} />
+          <Route path="/life" element={<LifePage />} />
+          <Route path='/profile' element={<ProfilePage />} />
         </Routes>
     </>
   )
